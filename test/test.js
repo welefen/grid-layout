f57480544d45b704b2808f3ccf83d8d1f84500f9
welefen <welefen@gmail.com>
2020-03-30 18:19:32 +0800

    add test files

---
+++ test/test.js
@@ -0,0 +1,23 @@
+import test from "ava";
+import { getdirFiles } from "think-helper";
+import path from "path";
+import { Container, Node } from "../lib/index";
+import fs from 'fs';
+
+const files = getdirFiles(path.join(__dirname, 'case'));
+console.log('files', files);
+
+files.forEach(file => {
+  test(file, t => {
+    const filepath = path.join(__dirname, 'case', file);
+    const data = JSON.parse(fs.readFileSync(filepath, 'utf8'));
+    const container = new Container(data.container);
+    data.items.forEach(item => {
+      const node = new Node(item);
+      container.appendChild(node);
+    })
+    container.calculateLayout();
+    const result = container.getAllComputedLayout();
+    t.deepEqual(result, data.result);
+  });
+});
