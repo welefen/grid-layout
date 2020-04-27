8514938facf8f7f88612cbdc8f341f379e80c558
welefen <welefen@gmail.com>
2020-04-27 15:45:18 +0800

    add lib

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'production',
+  entry: './src/index.ts',
+  module: {
+    rules: [
+      {
+        test: /\.ts?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
+  resolve: {
+    extensions: [ '.ts', '.js' ]
+  },
+  output: {
+    filename: 'gridLayout.js',
+    path: path.resolve(__dirname, 'dist'),
+    library: 'GridLayout',
+    libraryTarget: 'umd',
+  },
+  externals: {
+    
+  }
+};
